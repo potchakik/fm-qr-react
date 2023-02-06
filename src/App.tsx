be380ce1b855b3fb7d150c4cdb77194cd744f8a6
Dan Suba <DESuba@abs-cbn.com>
2023-02-06 15:04:36 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <div>
+          <img
+            className="qr-image"
+            src="/src/assets/image-qr-code.png"
+            alt="qr-code"
+          />
+        </div>
+        <div className="text-container">
+          <p className="qr-text">
+            Improve your front-end skils by building projects.
+          </p>
+          <p className="qr-text-2">
+            Scan the QR code to view Frontend Mentor and take your coding skills
+            to the next level.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
